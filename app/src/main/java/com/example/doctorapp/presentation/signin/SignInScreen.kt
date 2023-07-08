83800f3da09a0a71e50cc3a8114c79400894a38b
miciox5 <matteo.miceli.5@gmail.com>
2023-07-08 20:56:24 +0200

    feat(image): add image logo signin/signup

--- app/src/main/java/com/example/doctorapp/presentation/signin/SignInScreen.kt
+++ app/src/main/java/com/example/doctorapp/presentation/signin/SignInScreen.kt
@@ -1,5 +1,6 @@
 package com.example.doctorapp.presentation.signin
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,10 +8,13 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Facebook
 import androidx.compose.material.icons.filled.Visibility
@@ -29,12 +33,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.doctorapp.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -50,8 +56,15 @@ fun SignInScreen(
         verticalArrangement = Arrangement.spacedBy(32.dp)
     ) {
         Column(
-            modifier = Modifier
+            modifier = Modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.medicapp_vector),
+                contentDescription = "",
+                modifier = Modifier.size(200.dp,200.dp)
+            )
             Text(
                 text = "Welcome!",
                 style = MaterialTheme.typography.headlineLarge,
--- app/src/main/java/com/example/doctorapp/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/doctorapp/presentation/signup/SignUpScreen.kt
@@ -1,13 +1,16 @@
 package com.example.doctorapp.presentation.signup
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
@@ -23,11 +26,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.doctorapp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,7 +50,13 @@ fun SignUpScreen(
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.medicapp_vector),
+                contentDescription = "",
+                modifier = Modifier.size(200.dp,200.dp)
+            )
             Text(
                 text = "Hi!",
                 style = MaterialTheme.typography.headlineLarge
